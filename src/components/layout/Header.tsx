28f23d248876c202691465a1d70b8d0af615ee35
Guilherme Campos <guilhermev272@gmail.com>
2026-01-12 15:42:02 -0300

    feat(header): add toggle button to mobile version

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,12 +9,17 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { CodeXml, FileDown } from "lucide-react";
+import { CodeXml, FileDown, Menu } from "lucide-react";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
+import { useSidebar } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+import { items } from "@/constants/menuItems";
 
 export function Header() {
-const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const { toggleSidebar } = useSidebar();
+  const pathname = usePathname();
 
   function toggleLanguage() {
     const next = i18n.language === "pt" ? "en" : "pt";
@@ -22,49 +27,84 @@ const { t, i18n } = useTranslation();
   }
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full px-6 py-4 bg-background/70 backdrop-blur-md border-b border-border">
+    <header className="fixed top-0 left-0 z-50 w-full px-6 py-4 bg-header backdrop-blur-md border-b border-border">
       <div className="flex items-center justify-between">
         {/* Logo */}
-        <Button asChild variant="ghost">
-          <Link
-            href="/"
-            className="flex items-center gap-2 font-semibold hover:text-primary"
+        <div className="flex gap-2 items-center">
+          <Button
+            className="lg:hidden"
+            onClick={toggleSidebar}
+            size="icon"
+            variant="hover"
+            asChild
           >
-            <CodeXml className="w-5 h-5 opacity-80" />
-            Guilherme Campos
-          </Link>
-        </Button>
+            <Menu className="w-5 h-5" />
+          </Button>
+
+          <Button asChild variant="hover">
+            <Link
+              href="/"
+              className="flex items-center gap-2 font-semibold lg:hover:scale-105 transition-transform"
+            >
+              <CodeXml className="w-5 h-5 opacity-80" />
+              Guilherme Campos
+            </Link>
+          </Button>
+        </div>
 
         {/* Navigation */}
-        <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <nav
+          id="mobile-menu"
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hidden lg:flex"
+        >
           <NavigationMenu>
             <NavigationMenuList>
-              {["home", "projects", "about", "contact"].map((item) => (
-                <NavigationMenuItem key={item}>
-                  <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                    <Link
-                      href={item === "home" ? "/" : `/${item}`}
-                      className="border bg-background shadow-xs hover:bg-accent hover:text-primary dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+              {items.map((item) => {
+                const isActive = pathname === item.url;
+
+                return (
+                  <NavigationMenuItem key={item.title}>
+                    <NavigationMenuLink
+                      asChild
+                      data-active={isActive}
+                      aria-current={isActive ? "page" : undefined}
+                      className={navigationMenuTriggerStyle()}
                     >
-                      {t(`navigation.${item}`)}
-                    </Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              ))}
+                      <Link
+                        href={item.url}
+                        className="border hover:scale-105 hover:bg-accent dark:bg-input/30 border-input dark:hover:bg-input/50"
+                      >
+                        {t(`navigation.${item.title}`)}
+                      </Link>
+                    </NavigationMenuLink>
+                  </NavigationMenuItem>
+                );
+              })}
             </NavigationMenuList>
           </NavigationMenu>
         </nav>
 
         {/* Actions */}
-        <div className="flex items-center gap-2">
-          <ToggleTheme />
+        <div className="items-center gap-2 hidden sm:flex">
+          <div className="flex items-center gap-1">
+            <ToggleTheme />
 
-          <Button size="icon" variant="outline" onClick={toggleLanguage} className="hover:text-primary transition-transform">
-            {i18n.language?.toUpperCase()}
-          </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={toggleLanguage}
+              className="hover:scale-105 transition-transform"
+            >
+              {i18n.language?.toUpperCase()}
+            </Button>
+          </div>
 
           <Button asChild>
-            <Link href="/Guilherme_Campos_Frontend_Jr.pdf" download>
+            <Link
+              href="/Guilherme_Campos_Frontend_Jr.pdf"
+              className="hover:scale-105 transition-transform"
+              download
+            >
               <FileDown className="w-4 h-4" />
               {t("actions.downloadCv")}
             </Link>
