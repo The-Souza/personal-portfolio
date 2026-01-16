f76e925d5da76fa0567a6946a9cf7c6ebabc50ae
Guilherme Campos <guilhermev272@gmail.com>
2026-01-16 18:04:26 -0300

    feat(layout): add intro and change background

---
+++ src/components/layout/Backgound.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import PixelBlast from "@/components/ui/PixelBlast";
+
+export function Background() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        overflow: "hidden",
+      }}
+    >
+      <PixelBlast
+        variant="diamond"
+        pixelSize={4}
+        color="#7f22fe"
+        patternScale={2}
+        patternDensity={1}
+        enableRipples
+        rippleSpeed={0.3}
+        rippleThickness={0.1}
+        rippleIntensityScale={1}
+        speed={0.5}
+        transparent
+        edgeFade={0.25}
+        className={undefined}
+        style={undefined}
+      />
+    </div>
+  );
+}
