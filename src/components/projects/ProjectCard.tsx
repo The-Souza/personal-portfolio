92149d99ca2740263ad68526f807131939b4269b
Guilherme Campos <guilhermev272@gmail.com>
2026-02-02 17:18:31 -0300

    refactor(projectCard): add new event key down

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Project } from "@/constants/projects-data";
 import {
@@ -28,7 +28,12 @@ export function ProjectCard({ project, onSelect }: ProjectCardProps) {
       tabIndex={0}
       onClick={onSelect}
       aria-label={t(project.titleKey)}
-      onKeyDown={(e) => e.key === "Enter" && onSelect()}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          e.preventDefault();
+          onSelect();
+        }
+      }}
       className="z-1 pt-0 gap-4 group flex flex-col justify-between transition-all duration-300 ease-out hover:-translate-y-1 hover:border-primary shadow-md hover:shadow-primary"
     >
       <div className="relative aspect-video w-full max-h-48 sm:max-h-52 lg:max-h-56 xl:max-h-60 overflow-hidden bg-muted rounded-t-xl">
@@ -57,7 +62,11 @@ export function ProjectCard({ project, onSelect }: ProjectCardProps) {
         {project.highlights && (
           <div className="flex flex-wrap gap-2">
             {project.highlights.map((badge) => (
-              <Badge key={badge} variant="secondary" className="border-border py-1 px-3">
+              <Badge
+                key={badge}
+                variant="secondary"
+                className="border-border py-1 px-3"
+              >
                 {t(badge)}
               </Badge>
             ))}
@@ -69,7 +78,10 @@ export function ProjectCard({ project, onSelect }: ProjectCardProps) {
 
       <CardFooter className="flex items-center justify-between">
         {project.status && (
-          <Badge variant={statusConfig[project.status].variant} className="border-border py-1 px-3">
+          <Badge
+            variant={statusConfig[project.status].variant}
+            className="border-border py-1 px-3"
+          >
             {t(`projects.data.${project.id}.status.${project.status}`)}
           </Badge>
         )}
