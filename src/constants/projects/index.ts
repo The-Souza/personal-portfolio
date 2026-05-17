81b53f7971901e92c0ca4a3a691ada083a549c0e
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-05-17 18:09:11 -0300

    feat(projects): add consulta-nfe-cte project with PT/EN translations

--- src/constants/projects/index.ts
+++ src/constants/projects/index.ts
@@ -1,7 +1,9 @@
-import { personalPortfolioProject } from "./personal-portfolio";
 import { accountManagementProject } from "./account-management";
+import { consultaNfeCteProject } from "./consulta-nfe-cte";
+import { personalPortfolioProject } from "./personal-portfolio";
 
 export const projects = [
+  consultaNfeCteProject,
   personalPortfolioProject,
   accountManagementProject,
 ];
