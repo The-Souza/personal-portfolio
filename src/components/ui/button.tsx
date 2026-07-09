3008509e10acac168d99b96e801f10ef1cf1b9ba
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-07-09 17:01:00 -0300

    style(button): adjust dark mode default variant colors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-chart-5 dark:hover:bg-ring",
+        default:
+          "bg-primary dark:bg-chart-4 text-primary-foreground hover:bg-chart-5 dark:hover:bg-primary",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -34,7 +35,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 function Button({
