43395bfcd486044cec93a2bbe635a3856df3f918
Guilherme Campos <174835117+The-Souza@users.noreply.github.com>
2026-07-12 21:41:25 -0300

    style(contact): simplify ContactSection vertical padding

--- src/app/contact/_components/ContactSection.tsx
+++ src/app/contact/_components/ContactSection.tsx
@@ -1,10 +1,10 @@
-import { ContactHero } from "./ContactHero";
 import { ContactForm } from "./ContactForm";
+import { ContactHero } from "./ContactHero";
 import { ContactLinks } from "./ContactLinks";
 
 export function ContactSection() {
   return (
-    <div className="min-h-[80vh] w-full grid grid-cols-1 lg:grid-cols-2 pt-14 sm:pt-20 lg:pt-0 gap-12 items-center justify-between">
+    <div className="min-h-[80vh] w-full grid grid-cols-1 lg:grid-cols-2 py-14 sm:py-20 gap-12 items-center justify-between">
       <div className="flex flex-col gap-8">
         <ContactHero />
         <ContactLinks />
